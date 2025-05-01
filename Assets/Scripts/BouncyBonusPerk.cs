86ef75599c92234dd039d030636134f4c6ae3c75
ajfrock2 <austfrock@gmail.com>
2025-05-01 02:14:19 -0700

    Added new level, nearly finished bouncy bird
    New scene, WIP level. Bouncy bird fixes and changes. Adjusted a single perk

--- Assets/Scripts/BouncyBonusPerk.cs
+++ Assets/Scripts/BouncyBonusPerk.cs
@@ -1,7 +1,21 @@
+using System;
 using UnityEngine;
 
 public class BouncyBonusPerk : MonoBehaviour
 {
-    //TODO 
-    //When bouncy bird collides with pig, do some bonus
+    
+    private void Start()
+    {
+        BouncyBird.OnBouncyBirdKill+= BouncyBirdOnOnBouncyBirdKill;
+    }
+
+    private void BouncyBirdOnOnBouncyBirdKill()
+    {
+        //TODO add a reroll, ----REROLL AND SHOTS LEFT NEED TO BE MOVED OFF OF SLINGSHOT AND ON TO THE BAG 
+    }
+
+    private void OnDestroy()
+    {
+        BouncyBird.OnBouncyBirdKill -= BouncyBirdOnOnBouncyBirdKill;
+    }
 }
