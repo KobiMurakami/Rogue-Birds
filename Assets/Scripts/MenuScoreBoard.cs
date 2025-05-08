1f8c2320bfd33df51098ef4f0660a391ae40ac29
KobiMurakami <151149985+KobiMurakami@users.noreply.github.com>
2025-05-08 15:53:29 -0700

    Fixed Scoring and Loadout Progression

--- Assets/Scripts/MenuScoreBoard.cs
+++ Assets/Scripts/MenuScoreBoard.cs
@@ -6,15 +6,18 @@ public class MenuScoreBoard : MonoBehaviour
 
     public TextMeshProUGUI HighScoreNumber;
     public TextMeshProUGUI HighLevelNumber;
+    public TextMeshProUGUI CumulativeScoreNumber;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         int highScore = PlayerPrefs.GetInt("highscore", 0);
         int highestLevel = PlayerPrefs.GetInt("highestlevel", 0);
+        int cumulativeScore = PlayerPrefs.GetInt("cumscore", 0);
 
         HighScoreNumber.text = highScore.ToString();
         HighLevelNumber.text = highestLevel.ToString();
+        CumulativeScoreNumber.text = cumulativeScore.ToString();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -12,6 +12,7 @@ public class Scoring : MonoBehaviour
     public int numEnemiesInLevel;
     public int numEnemiesKilled;
     public int score;
+    public bool initializedScore = false;
 
     public TextMeshProUGUI ScoreNumberText;
     
@@ -19,13 +20,23 @@ public class Scoring : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        score = PlayerPrefs.GetInt("currentScore", 0);
+        Debug.Log("Level Starting, fetching previous score");
+        score = PlayerPrefs.GetInt("currentscore", 0);
+        initializedScore = true;
+        Debug.Log("Previous Score fetched, it was " + score);
         Enemies.OnEnemyDeath += EnemyOnEnemyDeath;
+
+        AbilityGenerator.OnBonusChosen += BonusOnBonusChosen;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!initializedScore) {
+            score = PlayerPrefs.GetInt("currentscore", 0);
+            initializedScore = true;
+            Debug.Log("Initialized Score in update method");
+        }
         ScoreNumberText.text = score.ToString();
     }
 
@@ -37,5 +48,10 @@ public class Scoring : MonoBehaviour
         else {
             // Other enemy types
         }
+    }
+
+    void BonusOnBonusChosen() {
+        score = PlayerPrefs.GetInt("currentscore", 0);
+        initializedScore = true;
     }
 }
